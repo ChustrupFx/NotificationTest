9417dcb0b622a8d59af784241d1823884b97ebf1
Victor Inácio <chustrupgamer@gmail.com>
2023-10-27 15:05:43 -0300

    Initial Commit

---
+++ NotificationTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotificationTest
+//
+//  Created by Victor Soares on 27/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotificationTest/NotificationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationTestApp.swift
+//  NotificationTest
+//
+//  Created by Victor Soares on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
