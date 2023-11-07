66c7b96efffb09673f37a58d10a1494b4f350ada
Victor Inácio <chustrupgamer@gmail.com>
2023-11-07 14:44:46 -0300

    Full

--- NotificationTest/ContentView.swift
+++ NotificationTest/ContentView.swift
@@ -6,16 +6,113 @@
 //
 
 import SwiftUI
+import TipKit
+
+struct FavoriteLandmarkTip: Tip {
+    var title: Text {
+        Text("Save as a Favorite")
+    }
+    
+    
+    var message: Text? {
+        Text("Your favorite landmarks always appear at the top of the list.")
+    }
+    
+    
+    var image: Image? {
+        Image(systemName: "star")
+    }
+}
 
 struct ContentView: View {
+    @State var minutes: Int = 0
+    @State var seconds: Int = 0
+    
+    let favoriteLandmarkTip = FavoriteLandmarkTip()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            TipView(favoriteLandmarkTip)
+            
+            Text("Daqui quanto tempo a notificação vai ativar?")
+                .font(.title2)
+            
+            
+            
+            HStack {
+                Text("Minutos: ")
+                Picker("Minutes", selection: $minutes) {
+                    ForEach(0..<61) { index in
+                        Text(String(index))
+                            .tag(index)
+                    }
+                }
+            }
+            
+            HStack {
+                Text("Segundos: ")
+                Picker("Segundos", selection: $seconds) {
+                    ForEach(0..<61) { index in
+                        Text(String(index))
+                            .tag(index)
+                    }
+                }
+            }
+        
+            Button("Agendar") {
+
+                
+                let notificationContent = getNotificationContent()
+                
+                print(notificationContent)
+                triggerNotification(notificationContent)
+            }
         }
         .padding()
+        .onAppear {
+            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (accepted, error) in
+                print("Accepted")
+            }
+        }
+        .task {
+            // Configure and load your tips at app launch.
+            try? Tips.configure([
+                .displayFrequency(.immediate),
+                .datastoreLocation(.applicationDefault)
+            ])
+        }
+        
+    }
+    
+    func triggerNotification(_ content: UNMutableNotificationContent) {
+        
+        let uuid = UUID().uuidString
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(minutes * 60 + seconds), repeats: false)
+        
+        let request = UNNotificationRequest(identifier: uuid, content: content, trigger: trigger)
+        
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.add(request) { (error) in
+            
+            if error == nil {
+                print("Foi")
+            } else {
+                print(error)
+            }
+            
+        }
+        
+    }
+    
+    func getNotificationContent() -> UNMutableNotificationContent {
+        let content = UNMutableNotificationContent()
+        
+        content.title = "Notificação oi"
+        content.body = "Funcinou acho"
+        content.badge = 25
+        
+        return content
     }
 }
 
--- NotificationTest/NotificationTestApp.swift
+++ NotificationTest/NotificationTestApp.swift
@@ -7,11 +7,38 @@
 
 import SwiftUI
 
+class AppDelegate: NSObject, UIApplicationDelegate, ObservableObject {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        UIApplication.shared.registerForRemoteNotifications()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Error to register for temote notification with error: \(error)")
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print(deviceToken.hexString)
+    }
+}
+
 @main
 struct NotificationTestApp: App {
+    
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+extension Data {
+    var hexString: String {
+        let hexString = map { String(format: "%02.2hhx", $0) }.joined()
+        return hexString
     }
 }
